59ef5c5faa3ec89bb10a58d48c421f98c2dbfe8d
RakaRyG <rakaguntara10@gmail.com>
2022-06-26 13:20:20 +0700

    language

---
+++ includes/languages/ind.php
@@ -0,0 +1,10 @@
+<?php 
+
+const _REGISTER = "Registrasi";
+const _USERNAME = "Masukan username";
+const _EMAIL = "Masukan email";
+const _PASSWORD = "Masukan password";
+
+
+
+?>
--- registration.php
+++ registration.php
@@ -7,6 +7,21 @@
 <?php include "includes/navigation.php"; ?>
 
 <?php
+
+if (isset($_GET['lang']) && !empty($_GET['lang'])) {
+    $_SESSION['lang'] = $_GET['lang'];
+
+    if (isset($_SESSION['lang']) && $_SESSION['lang'] != $_GET['lang']) {
+        echo "<script type='text/javascript'>location.reload();</script>";
+    }
+}
+
+if (isset($_SESSION['lang'])) {
+    include "./includes/languages/" . $_SESSION['lang'] . ".php";
+} else {
+    include "./includes/languages/eng.php";
+}
+
 if (isset($_POST['submit'])) {
     $username = trim($_POST['username']);
     $email = trim($_POST['email']);
@@ -51,31 +66,43 @@ if (isset($_POST['submit'])) {
 
 <!-- Page Content -->
 <div class="container">
+    <form action="" class="navbar-form navbar-right" method="get" id="language_form">
+        <div class="form-group">
+            <select name="lang" class="form-control" onchange="changeLangueage()">
+                <option value="eng" <?php if (isset($_SESSION['lang']) && $_SESSION['lang'] == 'eng') {
+                                        echo "selected";
+                                    } ?>>English</option>
+                <option value="ind" <?php if (isset($_SESSION['lang']) && $_SESSION['lang'] == 'ind') {
+                                        echo "selected";
+                                    } ?>>Indonesia</option>
+            </select>
+        </div>
+    </form>
 
     <section id="login">
         <div class="container">
             <div class="row">
                 <div class="col-xs-6 col-xs-offset-3">
                     <div class="form-wrap">
-                        <h1>Register</h1>
+                        <h1><?php echo _REGISTER;?></h1>
                         <form role="form" action="registration.php" method="post" id="login-form" autocomplete="off">
                             <div class="form-group">
                                 <label for="username" class="sr-only">username</label>
-                                <input type="text" name="username" id="username" class="form-control" placeholder="Enter Desired Username">
+                                <input type="text" name="username" id="username" class="form-control" placeholder="<?php echo _USERNAME;?>">
                                 <p><?php echo isset($error['username']) ? $error['username'] : '' ?></p>
                             </div>
                             <div class="form-group">
                                 <label for="email" class="sr-only">Email</label>
-                                <input type="email" name="email" id="email" class="form-control" placeholder="somebody@example.com">
+                                <input type="email" name="email" id="email" class="form-control" placeholder="<?php echo _EMAIL;?>">
                                 <p><?php echo isset($error['email']) ? $error['email'] : '' ?></p>
                             </div>
                             <div class="form-group">
                                 <label for="password" class="sr-only">Password</label>
-                                <input type="password" name="password" id="key" class="form-control" placeholder="Password">
+                                <input type="password" name="password" id="key" class="form-control" placeholder="<?php echo _PASSWORD;?>">
                                 <p><?php echo isset($error['password']) ? $error['password'] : '' ?></p>
                             </div>
 
-                            <input type="submit" name="submit" id="btn-login" class="btn btn-custom btn-lg btn-block" value="Register">
+                            <input type="submit" name="submit" id="btn-login" class="btn btn-primary btn-lg btn-block" value="Register">
                         </form>
 
                     </div>
@@ -86,6 +113,12 @@ if (isset($_POST['submit'])) {
 
 
     <hr>
+
+    <script>
+        function changeLangueage() {
+            document.getElementById('language_form').submit();
+        }
+    </script>
 
 
 
