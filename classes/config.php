39dec7b4b63fb6514b592a614ee3d221502ce07a
RakaRyG <rakaguntara10@gmail.com>
2023-04-28 13:59:36 +0700

    Create config.php

---
+++ classes/config.php
@@ -0,0 +1,11 @@
+<?php 
+
+class config{
+    const SMTP_HOST = 'smtp.mailtrap.io';
+    const SMTP_PORT = 2525;
+    const SMTP_USER = '80b98604b20da0';
+    const SMTP_PASSWORD = '19077cdcc00f36';
+}
+
+
+?>
